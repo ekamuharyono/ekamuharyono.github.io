3983dab97b6718d873208421e50fe0f3468109dc
ekamuharyono <ekamuharyono02@gmail.com>
2021-11-12 13:23:09 +0700

    silang

---
+++ ItamJayaLaundry/assets/js/script.js
@@ -0,0 +1,11 @@
+const menu = document.getElementById('menu');
+const menuButton = document.querySelectorAll('#menu-icon');
+const mobileNav = document.getElementById('mobile-nav');
+
+function handleMenu(){
+  menuButton[0].classList.toggle('hidden');
+  menuButton[1].classList.toggle('hidden');
+  mobileNav.classList.toggle('hidden');
+}
+
+menu.onclick = handleMenu;
